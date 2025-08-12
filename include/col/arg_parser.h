7a20e02aa0754684660e481d081aa02b1b2d1642
yurielie <yurielie39c@gmail.com>
2025-08-12 22:29:36 +0900

    公開する必要のない定義を detail 名前空間に移動
    * 前方宣言にも requires 指定が必要で DRY じゃないので、detail 名前空間の位置をいい感じにした

--- include/col/arg_parser.h
+++ include/col/arg_parser.h
@@ -14,13 +14,6 @@
 
 namespace col {
 
-    template <class T>
-    struct is_optional_type : std::false_type{};
-    template <class T>
-    struct is_optional_type<std::optional<T>> : std::true_type{};
-    template <class T>
-    inline constexpr bool is_optional_type_v = is_optional_type<T>::value;
-
     class FlagConfig
     {
         const char* m_name;
@@ -214,148 +207,160 @@ namespace col {
     };
 
 
-    template <class ...Configs, std::size_t ...Idx>
-    constexpr auto init_args_tuple_impl(const std::tuple<Configs...>& configs, std::index_sequence<Idx...>) noexcept
-    {
-        return std::make_tuple(std::pair<std::optional<typename Configs::value_type>, const Configs&>(std::nullopt, std::cref(std::get<Idx>(configs)))...);
-    }
+    namespace detail {
 
-    template <class ...Configs>
-    constexpr std::tuple<std::pair<std::optional<typename Configs::value_type>, const Configs&>...> init_args_tuple(const std::tuple<Configs...>& configs) noexcept
-    {
-        return init_args_tuple_impl(configs, std::index_sequence_for<Configs...>{});
-    }
+        template <class T>
+        struct is_optional_type : std::false_type{};
+        template <class T>
+        struct is_optional_type<std::optional<T>> : std::true_type{};
+        template <class T>
+        inline constexpr bool is_optional_type_v = is_optional_type<T>::value;
 
-    template <class T, class Config>
-    constexpr void set_default_if_defined_one(std::pair<std::optional<T>, const Config&>& p) noexcept
-    {
-        auto& val = p.first;
-        const auto& cfg = p.second;
-        if constexpr( std::is_same_v<Config, col::FlagConfig> )
+        template <class ...Configs, std::size_t ...Idx>
+        constexpr auto init_args_tuple_impl(const std::tuple<Configs...>& configs, std::index_sequence<Idx...>) noexcept
         {
-            if( !val.has_value() )
+            return std::make_tuple(std::pair<std::optional<typename Configs::value_type>, const Configs&>(std::nullopt, std::cref(std::get<Idx>(configs)))...);
+        }
+
+        template <class ...Configs>
+        constexpr std::tuple<std::pair<std::optional<typename Configs::value_type>, const Configs&>...> init_args_tuple(const std::tuple<Configs...>& configs) noexcept
+        {
+            return init_args_tuple_impl(configs, std::index_sequence_for<Configs...>{});
+        }
+
+        template <class T, class Config>
+        constexpr void set_default_if_defined_one(std::pair<std::optional<T>, const Config&>& p) noexcept
+        {
+            auto& val = p.first;
+            const auto& cfg = p.second;
+            if constexpr( std::is_same_v<Config, col::FlagConfig> )
             {
-                val = cfg.get_default_value();
+                if( !val.has_value() )
+                {
+                    val = cfg.get_default_value();
+                }
+            }
+            else
+            {
+                if( !val.has_value() )
+                {
+                    val = cfg.get_default_value();
+                }
             }
         }
-        else
+
+        template <std::size_t ...Idx, class ...Ts, class ...Configs>
+        constexpr void set_default_if_defined(std::tuple<std::pair<std::optional<Ts>, const Configs&>...>& configs, std::index_sequence<Idx...>) noexcept
         {
-            if( !val.has_value() )
-            {
-                val = cfg.get_default_value();
-            }
+            ( (set_default_if_defined_one(std::get<Idx>(configs))), ...);
         }
-    }
 
-    template <std::size_t ...Idx, class ...Ts, class ...Configs>
-    constexpr void set_default_if_defined(std::tuple<std::pair<std::optional<Ts>, const Configs&>...>& configs, std::index_sequence<Idx...>) noexcept
-    {
-        ( (set_default_if_defined_one(std::get<Idx>(configs))), ...);
-    }
-
-    template <class T, class Config>
-    constexpr bool check_args_tuple_initialized_one(const std::optional<T>& arg, const Config& cfg) noexcept
-    {
-        if constexpr(std::is_same_v<Config, OptionConfig<T>>)
+        template <class T, class Config>
+        constexpr bool check_args_tuple_initialized_one(const std::optional<T>& arg, const Config& cfg) noexcept
         {
-            if constexpr(is_optional_type_v<T>)
+            if constexpr(std::is_same_v<Config, OptionConfig<T>>)
             {
-                return !cfg.is_required() || (arg.has_value() && (*arg).has_value());
+                if constexpr(detail::is_optional_type_v<T>)
+                {
+                    return !cfg.is_required() || (arg.has_value() && (*arg).has_value());
+                }
+                else
+                {
+                    return arg.has_value();
+                }
             }
             else
             {
                 return arg.has_value();
             }
         }
-        else
+
+        template <class ...Ts, class ...Configs>
+        constexpr bool check_args_tuple_initialized(const std::tuple<std::pair<std::optional<Ts>, Configs>...>& t) noexcept
         {
-            return arg.has_value();
+            return std::apply(
+                [](const auto& ...p) noexcept
+                {
+                    return ( check_args_tuple_initialized_one(p.first, p.second) && ... && true);
+                },
+                t);
         }
-    }
 
-    template <class ...Ts, class ...Configs>
-    constexpr bool check_args_tuple_initialized(const std::tuple<std::pair<std::optional<Ts>, Configs>...>& t) noexcept
-    {
-        return std::apply(
-            [](const auto& ...p) noexcept
-            {
-                return ( check_args_tuple_initialized_one(p.first, p.second) && ... && true);
-            },
-            t);
-    }
-
-    template <class ...Ts, class ...Configs>
-    constexpr std::tuple<Ts...> make_init_tuple(std::tuple<std::pair<std::optional<Ts>, Configs>...>&& t) noexcept
-    {
-        return std::apply(
-            [](auto&& ...p) noexcept
-            {
-                return std::tuple<Ts...>{ std::move(*(p.first))... };
-            },
-            std::move(t));
-    }
-    
-    template <std::size_t Index, std::size_t ...Idx, class I, class S, class ...Ts, class ...Configs>
-    requires (std::input_iterator<I> && std::sentinel_for<S, I>)
-    constexpr auto matches_configs_impl(
-        I& iter, S last,
-        std::tuple<std::pair<std::optional<Ts>, const Configs&>...>& init_args
-    ) noexcept -> std::expected<bool, std::string>
-    {
-        auto& p = std::get<Index>(init_args);
-        auto& val = p.first;
-        const auto& config = p.second;
-
-        const std::string_view arg{*iter};
-        if( arg == config.get_name() )
+        template <class ...Ts, class ...Configs>
+        constexpr std::tuple<Ts...> make_init_tuple(std::tuple<std::pair<std::optional<Ts>, Configs>...>&& t) noexcept
         {
-            std::ranges::advance(iter, 1);
-            if constexpr( std::is_same_v<std::remove_cvref_t<decltype(config)>, FlagConfig> )
-            {
-                val = true;
-                return std::expected<bool, std::string>{ true };
-            }
-            else
-            {
-                if( iter == last )
+            return std::apply(
+                [](auto&& ...p) noexcept
                 {
-                    return std::unexpected{ "no value given to option args" };
-                }
-                const auto res = config.call_converter(std::string_view{*iter}.data());
-                if( res.has_value() )
+                    return std::tuple<Ts...>{ std::move(*(p.first))... };
+                },
+                std::move(t));
+        }
+        
+        template <std::size_t Index, std::size_t ...Idx, class I, class S, class ...Ts, class ...Configs>
+        requires (std::input_iterator<I> && std::sentinel_for<S, I>)
+        constexpr auto matches_configs_impl(
+            I& iter, S last,
+            std::tuple<std::pair<std::optional<Ts>, const Configs&>...>& init_args
+        ) noexcept -> std::expected<bool, std::string>
+        {
+            auto& p = std::get<Index>(init_args);
+            auto& val = p.first;
+            const auto& config = p.second;
+
+            const std::string_view arg{*iter};
+            if( arg == config.get_name() )
+            {
+                std::ranges::advance(iter, 1);
+                if constexpr( std::is_same_v<std::remove_cvref_t<decltype(config)>, FlagConfig> )
                 {
-                    if( val.has_value() )
-                    {
-                        return std::unexpected("already initialized");
-                    }
-                    val = *res;
-                    std::ranges::advance(iter, 1);
+                    val = true;
                     return std::expected<bool, std::string>{ true };
                 }
                 else
                 {
-                    return std::unexpected{ res.error() };
+                    if( iter == last )
+                    {
+                        return std::unexpected{ "no value given to option args" };
+                    }
+                    const auto res = config.call_converter(std::string_view{*iter}.data());
+                    if( res.has_value() )
+                    {
+                        if( val.has_value() )
+                        {
+                            return std::unexpected("already initialized");
+                        }
+                        val = *res;
+                        std::ranges::advance(iter, 1);
+                        return std::expected<bool, std::string>{ true };
+                    }
+                    else
+                    {
+                        return std::unexpected{ res.error() };
+                    }
                 }
             }
+            if constexpr(sizeof...(Idx) > 0 )
+            {
+                return matches_configs_impl<Idx...>(iter, last, init_args);
+            }
+            else
+            {
+                return std::expected<bool, std::string>{ false };
+            }
         }
-        if constexpr(sizeof...(Idx) > 0 )
+
+        template <class I, class S, class ...Ts, class ...Configs, std::size_t ...Idx>
+        requires (std::input_iterator<I> && std::sentinel_for<S, I>)
+        constexpr std::expected<bool, std::string> matches_configs(I& iter, S last,
+            std::tuple<std::pair<std::optional<Ts>, const Configs&>...>& init_args,
+            std::index_sequence<Idx...>) noexcept
         {
             return matches_configs_impl<Idx...>(iter, last, init_args);
         }
-        else
-        {
-            return std::expected<bool, std::string>{ false };
-        }
-    }
 
-    template <class I, class S, class ...Ts, class ...Configs, std::size_t ...Idx>
-    requires (std::input_iterator<I> && std::sentinel_for<S, I>)
-    constexpr std::expected<bool, std::string> matches_configs(I& iter, S last,
-        std::tuple<std::pair<std::optional<Ts>, const Configs&>...>& init_args,
-        std::index_sequence<Idx...>) noexcept
-    {
-        return matches_configs_impl<Idx...>(iter, last, init_args);
-    }
+    } // namespace detail
+
 
     template <class ...Configs>
     class ArgParser
@@ -388,13 +393,13 @@ namespace col {
         requires (std::is_constructible_v<T, typename Configs::value_type...> && std::ranges::borrowed_range<R>)
         constexpr std::expected<T, std::string> parse(R range) const noexcept
         {
-            auto init_args = init_args_tuple(m_configs);
+            auto init_args = detail::init_args_tuple(m_configs);
             auto iter = std::ranges::cbegin(range);
             const auto last = std::ranges::cend(range);
 
             while( iter != last )
             {
-                const auto res = matches_configs(iter, last, init_args, std::index_sequence_for<Configs...>{});
+                const auto res = detail::matches_configs(iter, last, init_args, std::index_sequence_for<Configs...>{});
                 if( !res.has_value() )
                 {
                     return std::unexpected(res.error());
@@ -405,10 +410,10 @@ namespace col {
                 }
             }
 
-            set_default_if_defined(init_args, std::index_sequence_for<Configs...>{});
-            if( check_args_tuple_initialized(init_args) )
+            detail::set_default_if_defined(init_args, std::index_sequence_for<Configs...>{});
+            if( detail::check_args_tuple_initialized(init_args) )
             {
-                return std::expected<T, std::string>{ std::make_from_tuple<T>(make_init_tuple(std::move(init_args))) };
+                return std::expected<T, std::string>{ std::make_from_tuple<T>(detail::make_init_tuple(std::move(init_args))) };
             }
             return std::unexpected("args uninitialized");
         }
@@ -417,6 +422,4 @@ namespace col {
     template <class ...Configs>
     ArgParser(Configs...) -> ArgParser<Configs...>;
 
-
-
-}
+} // namespace col
