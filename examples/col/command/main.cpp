a58beaf15339bfdbd4302029f464feaf2263bb39
yurielie <yurielie39c@gmail.com>
2025-11-06 00:47:54 +0900

    command.h の example/README を追従

--- examples/col/command/main.cpp
+++ examples/col/command/main.cpp
@@ -30,28 +30,28 @@ int main(int argc, char** argv)
         // サブコマンドを持つ構造体も再帰的にサブコマンドに指定できます。
         std::variant<std::monostate, SubCmd1, SubCmd2> subcmd;
         bool version;
-        bool help;
+        bool verbose;
     };
 
     // コマンドを定義します。
     // Builder Pattern で定義できます。
     // それぞれの引数の型の値を定義順に並べたとき、パース結果の構造体を構築できる必要があります。
     constexpr auto parser = col::Cmd{"cmd", "sample command"}
-        .add(col::Arg{"--version", "show version"}) // 指定しなければ T = bool と見做されます。
-        .add(col::Arg<bool>{"--help", "show help"}) // 明示的に型を指定できます。
+        .add(col::Arg{"version", "show version"}) // 指定しなければ T = bool と見做されます。
+        .add(col::Arg<bool>{"verbose", "show verbose"}) // 明示的に型を指定できます。
         .add(col::SubCmd<SubCmd1>{"subcmd1", "subcommand 1"} // サブコマンドに対しては、対応させる構造体の型を指定します。
             .add(col::SubCmd<SubSubCmd>{"subsubcmd", "subcommand of subcmd1"} // サブコマンドの入れ子が可能です。
-                .add(col::Arg{"--num", "number"}
+                .add(col::Arg{"num", "number"}
                     .set_default(1) // 指定したデフォルト値から T が推論されます(この例では T = int)。
                 ) // T が整数型・浮動小数点数型の場合、パーサーを指定しなければ std::from_chars() に基づく規定のパーサーが利用されます。
             )
-            .add(col::Arg{"--str_opt", "string option as std::optional<std::string>"}
+            .add(col::Arg{"str_opt", "string option as std::optional<std::string>"}
                 .set_value_type<std::optional<std::string>>() // set_value_type<T>() でも型を明示的に指定できます。
                 .set_default(".") // T が指定されていても、T を構築できるデフォルト値であれば指定できます。
             )
         )
         .add(col::SubCmd<SubCmd2>{"subcmd2", "subcommand 2"}
-            .add(col::Arg{"--str", "string option as std::string"}
+            .add(col::Arg{"str", "string option as std::string"}
                 // パーサーの戻り値から T が推論されます。 std::otional<T>, std::expected<T, E> の場合はその有効値になります(この例では T = std::string)。
                 // パーサーを指定する場合は const char* で呼び出し可能である必要があります。
                 // パーサーの戻り値型には T, std::optional<T>, std::expected<T, E> (requires std::convertible_to<E, col::ParseError>) が指定できます。
@@ -65,7 +65,7 @@ int main(int argc, char** argv)
                     {
                         return std::unexpected{
                             col::ConverterConvertionError{
-                                .name = "--str",
+                                .name = "str",
                                 .arg = arg,
                             }
                         };
@@ -85,12 +85,6 @@ int main(int argc, char** argv)
     if( res.has_value() )
     {
         const auto cmd = *res;
-        if( cmd.help )
-        {
-            // get_usage() でコマンド体系を説明する文字列が得られます。
-            // 引数で最初のインデント数とインデント幅を指定できます。空白単位です。
-            std::println("usage:\n{}\n", parser.get_usage(2ZU, 4ZU));
-        }
         std::println("[cmd] version = {}", cmd.version);
         switch( cmd.subcmd.index() )
         {
@@ -129,12 +123,14 @@ int main(int argc, char** argv)
     }
     else
     {
-        // col::ParseError の実体は std::variant のため、
-        // std::visit を使って各エラー型に応じた処理が可能です。
-        // 各エラー型は std::formatter を特殊化しており、文字列として表示できます。
+        // col::ParseError の実体は std::variant です。
+        // 各エラー型は std::formatter を特殊化しており、文字列表示できます。
+        // 
+        // 各コマンドおよびサブコマンドにはヘルプオプション("--help") が自動実装されます。指定されると、 col::ShowHelp が返ります。
+        // ヘルプメッセージの表示は std::format や col::ShowHelp::help_message を利用します。
         std::visit([](const auto& e) static
             {
-                std::println("error: {}", e);
+                std::println("{}", e);
             }, res.error());
     }
 }
